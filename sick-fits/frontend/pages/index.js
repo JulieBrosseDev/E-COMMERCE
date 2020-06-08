67bf2bf61eacb20ad8e157c49ca2556198a5d279
Julie Brosse <privatejuliedev@gmail.com>
2020-06-08 14:50:47 +0200

    home and sell page created with next

---
+++ sick-fits/frontend/pages/index.js
@@ -0,0 +1,12 @@
+import Link from 'next/link';
+
+const Home = props=> (
+    <div>
+    <p>pakcspk</p>
+    <Link href="/sell">
+        <a>SELL IT</a>
+    </Link>
+    </div>
+)
+
+export default Home ;
---
+++ sick-fits/frontend/pages/sell.js
@@ -0,0 +1,15 @@
+// with next, no need to import react. it is done automaticaly
+
+import Link from 'next/link';
+
+const Sell = props=> (
+    <div>
+    <p>pakcspk</p>
+    {/* Link allows not to refresh the page */}
+    <Link href="/">
+        <a>HOME</a>
+    </Link>
+    </div>
+)
+
+export default Sell ;
